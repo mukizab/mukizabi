530d0855476e596ae2bf19391f2246fdad572b4b
mukizab <58148171+mukizab@users.noreply.github.com>
2019-11-24 14:01:31 -0800

    First commit
    the assignment

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salaries sal=new Salaries();
+            sal.empl_id = Convert.ToInt32(Console.ReadLine());
+            sal.empl_name = Console.ReadLine();
+            sal.empl_position = Console.ReadLine();
+            sal.empl_gender = Console.ReadLine();
+
+            double grossSalaries;
+            grossSalaries = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine($"{sal.empl_name} MonthlySalary for {sal.empl_position} is {sal.calculateSalaries(grossSalaries)}");
+             Console.WriteLine($"{sal.empl_name} OneTimeSalary for {sal.empl_position} is {sal.calculateSalaries(grossSalaries)}");
+              Console.WriteLine("your rate");
+              double rate;
+              rate = Convert.ToDouble(Console.ReadLine());
+              double hour;
+              hour = Convert.ToDouble(Console.ReadLine());
+              if (rate != 0 && hour != 0)
+              {
+                  Console.WriteLine($"{sal.empl_name} Hourly salary is {sal.calculateSalaries(rate,hour)}");
+              }
+              else
+              {
+                  Console.WriteLine("Hourly salary is 0");
+              }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Salaries.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp2
+{
+    public class Salaries
+    {
+        public int empl_id;
+        public string empl_name;
+        public string empl_position;
+        public string empl_gender;
+
+        public double calculateSalaries(double grossSalaries)
+        {
+            double Salaries;
+            Salaries = grossSalaries - ((grossSalaries * 0.3) + (grossSalaries * 0.05) + (grossSalaries * 0.03));
+            return Salaries;
+            
+
+        }
+
+        public double calculateSalaries(double rate, double hour)
+        {
+            double Salaries;
+            Salaries = ((rate * hour) - (((rate * hour) * 0.05) + (rate * hour) * 0.03));
+            return Salaries;
+            
+        }
+
+        public double calculateSalaries(int grossSalaries)
+        { 
+             double salaries;
+            salaries = grossSalaries - (grossSalaries * 0.3);
+            return salaries;
+        }
+            
+            
+    }
+}
